8620dd3f663f7decba8eeab3f2298ec131af5077
huseyinsalmansoftdev <huseyinsalmansoftdev@gmail.com>
2023-12-17 12:52:24 -0800

    fix the fetch url

--- src/modules/sudokumodule/SudokuModule.jsx
+++ src/modules/sudokumodule/SudokuModule.jsx
@@ -121,7 +121,7 @@ const SudokuModule = ({ gameMode, gameDifficulty, currentPage, setCurrentPage })
     useEffect(() => {
         const fetchSudokuData = async () => {
             try {
-                const response = await fetch('http://localhost:5000/sudoku', {
+                const response = await fetch('https://replit.com/@huseyinsalmanso/sudokuapp-backend/sudoku', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
